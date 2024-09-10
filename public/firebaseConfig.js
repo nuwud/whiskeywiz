5ad1b56e56371cd62f6c6f0aba663847d3f4e983
Patrick Allan Wood <nuwudorder@gmail.com>
2024-09-10 16:47:42 -0700

    Refactor Firebase configuration to remove duplicate code and update initialization in firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,13 +1,15 @@
-if (!firebase.apps.length) { const firebaseConfig = {
-  apiKey: "AIzaSyCqZMaGpJGf5veYgTXmytg1bLerva-of0U",
-  authDomain: "whiskeywiz2.firebaseapp.com",
-  databaseURL: "https://whiskeywiz2-default-rtdb.firebaseio.com",
-  projectId: "whiskeywiz2",
-  storageBucket: "whiskeywiz2.appspot.com",
-  messagingSenderId: "555320797929",
-  appId: "1:555320797929:web:0d4b062d7f2ab330fc1e78",
-  measurementId: "G-SK0TJJEPF5"
+if (!firebase.apps.length) { 
+  const firebaseConfig = {
+    apiKey: "AIzaSyCqZMaGpJGf5veYgTXmytg1bLerva-of0U",
+    authDomain: "whiskeywiz2.firebaseapp.com",
+    databaseURL: "https://whiskeywiz2-default-rtdb.firebaseio.com",
+    projectId: "whiskeywiz2",
+    storageBucket: "whiskeywiz2.appspot.com",
+    messagingSenderId: "555320797929",
+    appId: "1:555320797929:web:0d4b062d7f2ab330fc1e78",
+    measurementId: "G-SK0TJJEPF5"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig); }
+firebase.initializeApp(firebaseConfig); 
+}
