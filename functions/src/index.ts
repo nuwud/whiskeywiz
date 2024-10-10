d3bf34387bc718f01a1f89c998bd70ed68d9160f
Patrick Allan Wood <nuwudorder@gmail.com>
2024-10-10 11:50:50 -0700

    Not sure if this works yet. had to clean it up.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,12 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-//import {onRequest} from "firebase-functions/v2/https";
-//import * as logger from "firebase-functions/logger";
+// Uncomment and use these imports if needed
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
+// Uncomment and use this function if needed
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
