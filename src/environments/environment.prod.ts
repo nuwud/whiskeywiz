a9be5032be9e4f703ebdc027af1db3fa875b52f2
Patrick Allan Wood <nuwudorder@gmail.com>
2024-12-29 14:43:23 -0800

    Update environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,16 +3,20 @@ export const environment = {
   enableImageDebugging: false,
   buildTimestamp: Date.now(),
   firebase: {
-    apiKey: process.env.FIREBASE_API_KEY,
+    // These values are public and safe to commit
+    apiKey: "AIzaSyCqZMaGpJGf5veYgTXmytg1bLerva-of0U",
     authDomain: "whiskeywiz2.firebaseapp.com",
     databaseURL: "https://whiskeywiz2-default-rtdb.firebaseio.com",
     projectId: "whiskeywiz2",
     storageBucket: "whiskeywiz2.appspot.com",
     messagingSenderId: "555320797929",
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    appId: "1:555320797929:web:0d4b062d7f2ab330fc1e78",
+    measurementId: "G-SK0TJJEPF5"
   },
   shopify: {
-    apiUrl: 'https://blind-barrels.myshopify.com/admin/api/2024-01'
+    apiUrl: 'https://blind-barrels.myshopify.com/admin/api/2024-01',
+    // These will be loaded from Firebase config
+    apiKey: process.env.SHOPIFY_API_KEY,
+    apiSecret: process.env.SHOPIFY_API_SECRET
   }
 };
