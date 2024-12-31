78079b0bdfeaf7c84d9aebc423280317c0d2ee41
Patrick Allan Wood <nuwudorder@gmail.com>
2024-12-31 15:56:53 -0800

    Add analytics route and component to admin module with route guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
+import { canActivateAdmin } from '../guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent }
+  {
+    path: '',
+    component: AdminComponent,
+    canActivate: [canActivateAdmin],
+    children: [
+      {
+        path: 'analytics',
+        component: AnalyticsComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AdminComponent } from './admin.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
 import { SharedModule } from '../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminNavModule } from '../admin-nav/admin-nav.module';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AnalyticsComponent
   ],
   imports: [
     CommonModule,
