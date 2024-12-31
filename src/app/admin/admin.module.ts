b5b771e43f8c31cd692053c77d70231287e7bd23
Patrick Allan Wood <nuwudorder@gmail.com>
2024-12-30 16:36:13 -0800

    Create AdminModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,20 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AdminComponent } from './admin.component';
-import { AdminNavComponent } from '../admin-nav/admin-nav.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    AdminComponent,
-    AdminNavComponent
+    AdminComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   exports: [
-    AdminComponent,
-    AdminNavComponent
+    AdminComponent
   ]
 })
 export class AdminModule { }
