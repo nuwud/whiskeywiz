fd2214659b819ce17f70816281818d9d4283528f
Patrick Allan Wood <nuwudorder@gmail.com>
2024-12-31 16:24:52 -0800

    fix: clean up import statements in game-elements.ts

--- src/app/elements/game-elements.ts
+++ src/app/elements/game-elements.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { createCustomElement } from '@angular/elements';
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '../shared/shared.module'; 
 import { CoreModule } from '../core/core.module';
 import { GameComponent } from '../shared/game/game.component';
 import { CommonModule } from '@angular/common';
