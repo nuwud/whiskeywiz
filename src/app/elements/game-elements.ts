bed30d33b38f06b424ceb6c1a5b0c990e2cece54
Patrick Allan Wood <nuwudorder@gmail.com>
2024-12-31 16:21:21 -0800

    fix: remove duplicate import of BrowserAnimationsModule and add CommonModule to game-elements.ts

--- src/app/elements/game-elements.ts
+++ src/app/elements/game-elements.ts
@@ -1,11 +1,12 @@
 import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { createCustomElement } from '@angular/elements';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
 import { GameComponent } from '../shared/game/game.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
