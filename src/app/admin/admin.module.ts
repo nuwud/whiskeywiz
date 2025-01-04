71adc925058fe160fb67a2bbc7baaf194c4219d9
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 12:30:46 -0800

    fix: add missing providers to admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,10 @@ import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnalyticsComponent } from './analytics/analytics.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { FirebaseService } from '../services/firebase.service';
+import { AuthService } from '../services/auth.service';
+import { GameService } from '../services/game.service';
+import { ScoreService } from '../services/score.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,12 @@ import { AdminRoutingModule } from './admin-routing.module';
     ReactiveFormsModule,
     SharedModule,
     AdminRoutingModule
+  ],
+  providers: [
+    FirebaseService,
+    AuthService,
+    GameService,
+    ScoreService
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
