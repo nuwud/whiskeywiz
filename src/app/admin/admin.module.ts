0df767eca1fa06edb5d1918b0f7694eba9efcf34
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-03 17:23:27 -0800

    fix: update admin module with proper shared module import

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminComponent } from './admin.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
 import { SharedModule } from '../shared/shared.module';
-import { AdminRoutingModule } from './admin-routing.module';
-import { AdminNavModule } from '../admin-nav/admin-nav.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AdminComponent,
-    AnalyticsComponent
+    AdminComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    SharedModule,
-    AdminRoutingModule,
-    AdminNavModule
+    SharedModule
+  ],
+  exports: [
+    AdminComponent
   ]
 })
 export class AdminModule { }
