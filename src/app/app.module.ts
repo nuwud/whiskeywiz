f9175eec11f1a9054c29df3b0050445c5ddd786b
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-03 17:20:24 -0800

    fix: update app module with component schemas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { QuarterComponent } from './quarters/quarter.component';
+import { BaseQuarterComponent } from './quarters/base-quarter.component';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { AuthButtonComponent } from './auth/auth-button/auth-button.component';
-import { CoreModule } from './core/core.module';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
-import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
-import { environment } from '../environments/environment';
-import { AuthGuard } from './guards/auth.guard';
-import { PlayerComponent } from './player/player.component';
-import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
-
-export const FIREBASE_APP = AngularFireModule.initializeApp(environment.firebase);
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    AuthButtonComponent,
-    PlayerComponent,
-    PageNotFoundComponent
+    QuarterComponent,
+    BaseQuarterComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedModule,
-    CoreModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule,
-    AngularFireDatabaseModule,
-    AngularFireFunctionsModule,
-    AngularFireAnalyticsModule
+    SharedModule
   ],
-  providers: [AuthGuard],
+  providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
