3a5cadeba96f3bfe4adc7812b278feeda99daf88
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 14:16:07 -0800

    fix: update admin module with quarter edit component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,13 +5,15 @@ import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnalyticsComponent } from './analytics/analytics.component';
 import { AdminQuartersComponent } from './quarters/admin-quarters.component';
+import { AdminQuarterEditComponent } from './quarters/admin-quarter-edit.component';
 import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [
     AdminComponent,
     AnalyticsComponent,
-    AdminQuartersComponent
+    AdminQuartersComponent,
+    AdminQuarterEditComponent
   ],
   imports: [
     CommonModule,
