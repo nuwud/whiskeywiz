0a864a72afbbe7583469786b660dd0420431235f
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 14:49:16 -0800

    fix: update game module with required components and dependencies

--- src/app/shared/game/game.module.ts
+++ src/app/shared/game/game.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GameComponent } from './game.component';
+import { StarRatingComponent } from '../components/star-rating/star-rating.component';
+import { ResultsComponent } from '../results/results.component';
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [
+    GameComponent,
+    StarRatingComponent,
+    ResultsComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule
+    ReactiveFormsModule,
+    RouterModule,
+    BrowserAnimationsModule
   ],
-  exports: [GameComponent]
+  exports: [
+    GameComponent,
+    StarRatingComponent,
+    ResultsComponent
+  ]
 })
 export class GameModule { }
