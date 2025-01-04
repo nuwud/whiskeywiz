08dbaaffb765f39a8f03c4e8e6083df2c831f268
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 12:22:21 -0800

    fix: update admin module to use admin routing module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,23 +1,10 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnalyticsComponent } from './analytics/analytics.component';
-import { canActivateAdmin } from '../guards/auth.guard';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: AdminComponent,
-    canActivate: [canActivateAdmin],
-    children: [
-      { path: 'analytics', component: AnalyticsComponent },
-      { path: '', redirectTo: 'quarters', pathMatch: 'full' }
-    ]
-  }
-];
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
   declarations: [
@@ -29,7 +16,7 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
-    RouterModule.forChild(routes)
+    AdminRoutingModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
