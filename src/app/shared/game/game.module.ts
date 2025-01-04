015a33bac08c84cad168e171a1fab79aa1687fc5
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 12:11:00 -0800

    fix: remove duplicate routes from GameModule

--- src/app/shared/game/game.module.ts
+++ src/app/shared/game/game.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { GameComponent } from './game.component';
-
-const routes: Routes = [
-  { path: 'game', component: GameComponent },
-  { path: 'player', component: GameComponent }
-];
 
 @NgModule({
   declarations: [GameComponent],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild(routes)
+    RouterModule
   ],
-  exports: [GameComponent, RouterModule]
+  exports: [GameComponent]
 })
 export class GameModule { }
