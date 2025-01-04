da53057fea729fcde7abd0a39bda9ea32b4cc0b1
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-03 17:27:42 -0800

    fix: update shared module with required services and RouterModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { GameComponent } from './game/game.component';
 import { GameBannerComponent } from './components/game-banner/game-banner.component';
 import { StarRatingComponent } from './components/star-rating/star-rating.component';
 import { ResultsComponent } from './results/results.component';
+
+// Services
+import { DataCollectionService } from '../services/data-collection.service';
+import { ScoreService } from '../services/score.service';
+import { GameService } from '../services/game.service';
 
 const COMPONENTS = [
   GameComponent,
@@ -20,9 +26,15 @@ const COMPONENTS = [
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [...COMPONENTS],
+  providers: [
+    DataCollectionService,
+    ScoreService,
+    GameService
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
