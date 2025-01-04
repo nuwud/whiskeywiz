a401a63c7d0dee09c1bb1a586a9c9f8063b44f9e
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-03 18:30:48 -0800

    fix: update September quarters to match template

--- src/app/quarters/0924/0924.component.ts
+++ src/app/quarters/0924/0924.component.ts
@@ -1,12 +1,9 @@
-import { Component, Inject, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BaseQuarterComponent } from '../base-quarter.component';
 import { AuthService } from '../../services/auth.service';
 import { FirebaseService } from '../../services/firebase.service';
 import { NgForm } from '@angular/forms';
-import { FirebaseApp } from '@angular/fire/app';
-import { FIREBASE_APP } from '../../app.module';
-import { AnalyticsService } from 'src/app/services/analytics.service';
-
+import { AnalyticsService } from '../../services/analytics.service';
 
 @Component({
   selector: 'app-quarter-0924',
@@ -22,33 +19,24 @@ import { AnalyticsService } from 'src/app/services/analytics.service';
 })
 export class Q0924Component extends BaseQuarterComponent {
   @Input() override quarterId: string = '0924';
-  @Input() quarterName: string = 'September 2024';
+  @Input() override quarterName: string = 'September 2024';
 
   constructor(
-    @Inject(FIREBASE_APP) app: FirebaseApp,
     firebaseService: FirebaseService,
     authService: AuthService,
-    analyticsService: AnalyticsService,
-    private changeDetectorRef: ChangeDetectorRef
+    analyticsService: AnalyticsService
   ) {
-    super(app, firebaseService, authService, analyticsService);
-    this.quarterId = '0924';
+    super(firebaseService, authService, analyticsService);
   }
 
   onSubmit(form: NgForm) {
-    if (!this.app) {
-      console.error('Firebase not initialized');
-      return;
-    }
-    
     if (form.valid) {
       const guess = {
         age: form.value.ageGuess,
         proof: form.value.proofGuess,
         mashbill: form.value.mashbillGuess
       };
-      super.submitGuess(guess);
-      this.changeDetectorRef.detectChanges();
+      this.submitGuess(guess);
     }
   }
 }
