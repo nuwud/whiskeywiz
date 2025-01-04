4c3f9f2e430b5a511d00b76d48e5e26899d9b8e7
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 09:25:40 -0800

    fix: add route guards for admin and game components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard, canActivateAdmin, canActivateGame } from './guards/auth.guard';
 import { GameComponent } from '../app/shared/game/game.component';
 import { PlayerComponent } from './player/player.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: GameComponent },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'game', component: GameComponent },
+  { 
+    path: 'admin', 
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canActivate: [canActivateAdmin]
+  },
+  { 
+    path: 'game', 
+    component: GameComponent,
+    canActivate: [canActivateGame]
+  },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: PageNotFoundComponent },  // Fallback route
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
