b8c46c04896198d9d53c769227e8f1e42bd0d8a6
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-04 14:46:08 -0800

    fix: update quarter component to match base class

--- src/app/quarters/0622/0622.component.ts
+++ src/app/quarters/0622/0622.component.ts
@@ -1,54 +1,23 @@
-import { Component, Inject, ChangeDetectorRef, Input } from '@angular/core';
-import { BaseQuarterComponent } from '../base-quarter.component';
-import { AuthService } from '../../services/auth.service';
+import { Component } from '@angular/core';
 import { FirebaseService } from '../../services/firebase.service';
-import { NgForm } from '@angular/forms';
-import { FirebaseApp } from '@angular/fire/app';
-import { FIREBASE_APP } from '../../app.module';
-import { AnalyticsService } from 'src/app/services/analytics.service';
-
+import { AuthService } from '../../services/auth.service';
+import { AnalyticsService } from '../../services/analytics.service';
+import { GameService } from '../../services/game.service';
+import { BaseQuarterComponent } from '../base-quarter.component';
 
 @Component({
   selector: 'app-quarter-0622',
-  template: `
-    <app-game-banner 
-      [quarterId]="quarterId"
-      [quarterName]="quarterName || 'June 2022'">
-    </app-game-banner>
-  `,
-  styles: [`
-    small { color: red; }
-  `]
+  template: `<ng-container *ngTemplateOutlet="baseQuarter.template"></ng-container>`
 })
 export class Q0622Component extends BaseQuarterComponent {
-  @Input() override quarterId: string = '0622';
-  @Input() quarterName: string = 'June 2022';
-
   constructor(
-    @Inject(FIREBASE_APP) app: FirebaseApp,
-    firebaseService: FirebaseService,
-    authService: AuthService,
-    analyticsService: AnalyticsService,
-    private changeDetectorRef: ChangeDetectorRef
+    protected override firebaseService: FirebaseService,
+    protected override authService: AuthService,
+    protected override analyticsService: AnalyticsService,
+    protected override gameService: GameService
   ) {
-    super(app, firebaseService, authService, analyticsService);
+    super(firebaseService, authService, analyticsService, gameService);
     this.quarterId = '0622';
-  }
-
-  onSubmit(form: NgForm) {
-    if (!this.app) {
-      console.error('Firebase not initialized');
-      return;
-    }
-    
-    if (form.valid) {
-      const guess = {
-        age: form.value.ageGuess,
-        proof: form.value.proofGuess,
-        mashbill: form.value.mashbillGuess
-      };
-      super.submitGuess(guess);
-      this.changeDetectorRef.detectChanges();
-    }
+    this.quarterName = 'Q2 2022';
   }
 }
