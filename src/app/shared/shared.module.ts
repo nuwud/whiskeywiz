b7c3f27e414f671fdfe6c4006d352a5928c7f90e
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-03 17:25:43 -0800

    fix: update shared module with complete component declarations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,29 +1,28 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Components
 import { GameComponent } from './game/game.component';
 import { GameBannerComponent } from './components/game-banner/game-banner.component';
 import { StarRatingComponent } from './components/star-rating/star-rating.component';
 import { ResultsComponent } from './results/results.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const COMPONENTS = [
+  GameComponent,
+  GameBannerComponent,
+  StarRatingComponent,
+  ResultsComponent
+];
 
 @NgModule({
-  declarations: [
-    GameComponent,
-    GameBannerComponent,
-    StarRatingComponent,
-    ResultsComponent
-  ],
+  declarations: [...COMPONENTS],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  exports: [
-    GameComponent,
-    GameBannerComponent,
-    StarRatingComponent,
-    ResultsComponent
-  ],
+  exports: [...COMPONENTS],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
