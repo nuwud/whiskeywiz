8dc5323fca798506f23689ff60b6156005dff37d
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-06 14:55:09 -0800

    Fix shared module - update module imports and exports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GameComponent } from './game/game.component';
-import { ResultsComponent } from './results/results.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    GameComponent,
-    ResultsComponent
+    GameComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   exports: [
     GameComponent,
-    ResultsComponent,
     FormsModule
   ]
 })
