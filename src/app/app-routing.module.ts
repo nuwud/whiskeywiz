a30002bbdcf86b82e49eebb5d0450787c162eed9
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-06 14:59:55 -0800

    Add app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { canActivateAdmin, canActivateGame } from './guards/auth.guard';
 import { GameComponent } from './shared/game/game.component';
-import { PlayerComponent } from './player/player.component';
-import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { 
-    path: '', 
-    redirectTo: 'quarters', 
-    pathMatch: 'full' 
+    path: 'game', 
+    component: GameComponent
   },
   { 
-    path: 'admin', 
+    path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-    canActivate: [canActivateAdmin]
-  },
-  {
-    path: 'quarters',
-    loadChildren: () => import('./quarters/quarters.module').then(m => m.QuartersModule)
+    canActivate: [authGuard]
   },
   { 
-    path: 'player', 
-    component: PlayerComponent,
-    canActivate: [canActivateGame]
-  },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '**', component: PageNotFoundComponent }
+    path: '', 
+    redirectTo: '/game', 
+    pathMatch: 'full' 
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: true,
-    scrollPositionRestoration: 'enabled',
-    anchorScrolling: 'enabled'
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
